9ff3e0306ef39086a98af409a35ffb384852fa75
ShivinDass <shivin16091@iiitd.ac.in>
2018-07-26 14:55:53 +0530

    Add files via upload

---
+++ WordLength.py
@@ -0,0 +1,28 @@
+#import os
+file1="words_alpha.txt"
+f= open(file1,"r")
+ln=0
+longestWord=""
+for word in f:
+    if ln<len(word):
+        ln=len(word)
+        longestWord=word
+f.seek(0)
+print(longestWord)
+
+file2="temp.txt"
+f2=open(file2,"w")
+for word in f:
+    wl=len(word)
+    if word[wl-1]=='\n':   
+        word=word[:wl-1]
+        wl=wl-1
+    for i in range(0,ln-wl):
+        word=word+'#'
+    word=word+'\n'
+    f2.write(word)
+
+f.close()
+f2.close()
+#delete(file1)
+#os.rename(file2,file1)
